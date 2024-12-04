8a5ca285189aac2c3e0f0d92dfc021a4ee30575a
kalin <kalin@KALINDESKTOP>
2024-12-03 23:44:29 -0700

    fixed build fail

--- src/Data/inventoryMutations.ts
+++ src/Data/inventoryMutations.ts
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { inventoryApiService } from "./inventoryService";
-import { InventoryDTO } from "./Interfaces";
+import { Inventory } from "./Interfaces";
 
 export const useUpdateInventoryMutation = () => {
     const queryClient = useQueryClient();
 
-    return useMutation<void, Error, InventoryDTO>({
+    return useMutation<void, Error, Inventory>({
         mutationFn: inventoryApiService.Put,
         onSuccess: () => {
             toast.success("Card updated successfully");
